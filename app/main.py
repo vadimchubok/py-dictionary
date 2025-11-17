f1bdef38109b62a8393bae64403f4aa926573b31
Vadim <vadimchubok@gmail.com>
2025-11-17 16:09:59 +0200

    Sollution

--- app/main.py
+++ app/main.py
@@ -12,46 +12,41 @@ class Dictionary:
         return self.count
 
     def __getitem__(self, key: Any) -> Any:
-        index = hash(key) % self.size
+        key_hash = hash(key)
+        index = key_hash % self.size
 
         while True:
             element = self.table[index]
             if element is None:
                 raise KeyError(key)
-            k, v = element
-            if k == key:
-                return v
-
+            stored_key, stored_hash, stored_value = element
+            if stored_hash == key_hash and stored_key == key:
+                return stored_value
             index = (index + 1) % self.size
 
     def __setitem__(self, key: Any, value: Any) -> None:
         if (self.count + 1) > self.size * (2 / 3):
             self._resize()
-
-        index = hash(key) % self.size
-
+        key_hash = hash(key)
+        index = key_hash % self.size
         while True:
             element = self.table[index]
-
             if element is None:
-                self.table[index] = (key, value)
+                self.table[index] = (key, key_hash, value)
                 self.count += 1
                 return
-            k, v = element
-            if k == key:
-                self.table[index] = (key, value)
+            stored_key, stored_hash, stored_value = element
+            if stored_hash == key_hash and stored_key == key:
+                self.table[index] = (key, key_hash, value)
                 return
-
             index = (index + 1) % self.size
 
     def _resize(self) -> None:
         old_table = self.table
-
         self.size *= 2
         self.table = [None] * self.size
         self.count = 0
-
         for element in old_table:
             if element is not None:
-                key, value = element
-                self[key] = value
+                stored_key, stored_hash, stored_value = element
+                self[stored_key] = stored_value
